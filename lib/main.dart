a3a13b58771a10c5c6898f5cf43194f0e342ee92
abhishek prajapati <abhishekprajapati.890e@gmail.com>
2023-03-16 23:52:48 +0530

    change color to red

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.red,
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
